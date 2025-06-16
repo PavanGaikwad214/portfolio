3d34709d6cc2f8e23acbe72c26644a84e0ec220b
Pavan Gaikwad <gaikwadpavan57@gmail.com>
2025-06-16 18:26:28 +0530

    Nothing changed

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ toggle.addEventListener('click',() => {
     body.classList.toggle('dark-mode');
 
     if(body.classList.contains('dark-mode')){
-        toggle.classList.remove('fa-moon');
         toggle.classList.add('fa-sun')
+        toggle.classList.remove('fa-moon');
     } else {
-        toggle.classList.remove('fa-sun');
         toggle.classList.add('fa-moon')
+        toggle.classList.remove('fa-sun');
     }
 })
 
